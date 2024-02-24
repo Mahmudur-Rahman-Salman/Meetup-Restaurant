4c236ff2ce54e7a7567ca65c3fed441a0da3fc11
Mahmudur Rahman Salman <mahmudur.salman@gmail.com>
2024-02-25 01:51:31 +0600

    useMenu hooks added

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,14 +1,20 @@
 import { Helmet } from "react-helmet-async";
-
+import Cover from "./Shared/Cover";
+import menuImg from "../assets/menu/banner3.jpg";
+// import soupImg from "../assets/menu/soup-bg.jpg";
+// import saladImg from "../assets/menu/salad-bg.jpg";
+// import pizzaImg from "../assets/menu/pizza-bg.jpg";
+// import dessertImg from "../assets/menu/dessert-bg.jpeg";
 
 const Menu = () => {
-    return (
-        <div>
-            <Helmet>
-                <title>Meetup | Menu</title>
-            </Helmet>
-        </div>
-    );
+  return (
+    <div>
+      <Helmet>
+        <title>Meetup | Menu</title>
+      </Helmet>
+      <Cover img={menuImg} title="our menu"></Cover>
+    </div>
+  );
 };
 
 export default Menu;
