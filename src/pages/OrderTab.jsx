47e3d6de22c7f867010dfbff70702f14a452f91e
Mahmudur Rahman Salman <mahmudur.salman@gmail.com>
2024-02-29 02:35:43 +0600

    reviews data added from database

--- src/pages/OrderTab.jsx
+++ src/pages/OrderTab.jsx
@@ -1,26 +1,31 @@
-// import { Swiper, SwiperSlide } from "swiper/react";
-// import { Pagination } from "swiper/modules";
-// import "swiper/css";
-// import "swiper/css/pagination";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
 import Foodcard from "./Foodcard";
 
 const OrderTab = ({ items }) => {
+  const pagination = {
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + "</span>";
+    },
+  };
   return (
     <div>
-      {/* <Swiper
-        Pagination={Pagination}
+      <Swiper
+        Pagination={pagination}
         modules={[Pagination]}
         className="mySwiper"
       >
         <SwiperSlide>
-          
+          <div className="grid md:grid-cols-2 lg:grid-cols-3 justify-center md:justify-center gap-10">
+            {items.map((item) => (
+              <Foodcard key={item._id} item={item}></Foodcard>
+            ))}
+          </div>
         </SwiperSlide>
-      </Swiper> */}
-      <div className="grid md:grid-cols-2 lg:grid-cols-3 justify-center md:justify-center gap-10">
-        {items.map((item) => (
-          <Foodcard key={item._id} item={item}></Foodcard>
-        ))}
-      </div>
+      </Swiper>
     </div>
   );
 };
